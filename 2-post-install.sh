ff0673c8c3d54e973f8d0be249cfb1827a894580
Nate Randa <naterandaofficial@gmail.com>
2021-11-26 11:52:47 -0500

    move microcode install so it's picked up by GRUB

--- 2-post-install.sh
+++ 2-post-install.sh
@@ -61,6 +61,21 @@ then
     echo -e "\nbootloader=$bootloader" >> /config.conf
 fi
 
+# Check processor type and install microcode (thanks CTT!)
+proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
+case "$proc_type" in
+	GenuineIntel)
+        echo "Installing Intel microcode..."
+		pacman -S --noconfirm intel-ucode
+		proc_ucode=intel-ucode.img
+		;;
+	AuthenticAMD)
+        echo "Installing AMD microcode..."
+		pacman -S --noconfirm amd-ucode
+		proc_ucode=amd-ucode.img
+		;;
+esac
+
 # Install bootloader
 case $bootloader in
     grub)
@@ -94,22 +109,7 @@ if [ ! -v username ]
 then
     read -p "Username: " username
     echo -e "\nusername=$username" >> /config.conf
-fi
-
-# Check processor type and install microcode (thanks CTT!)
-proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
-case "$proc_type" in
-	GenuineIntel)
-        echo "Installing Intel microcode..."
-		pacman -S --noconfirm intel-ucode
-		proc_ucode=intel-ucode.img
-		;;
-	AuthenticAMD)
-        echo "Installing AMD microcode..."
-		pacman -S --noconfirm amd-ucode
-		proc_ucode=amd-ucode.img
-		;;
-esac	
+fi	
 
 # Set up user
 echo "Setting up user..."
